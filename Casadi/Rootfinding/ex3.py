98e9bb15841b0d3cbad56f3feacfaaadfcda22dd
harry5081 <harry508189@gmail.com>
2023-05-01 11:22:31 -0400

    Finish ex3

--- Casadi/Rootfinding/ex3.py
+++ Casadi/Rootfinding/ex3.py
@@ -3,8 +3,8 @@
 import casadi as c
 import ex1 as ex1
 import ex2 as ex2
-
-rf = c.rootfinder('rf','newton',{'x': ex2.X,'g': ex1.g(ex2.X)},{'print_iteration': True})
+options = {'print_iteration': True}
+rf = c.rootfinder('rf','newton',{'x': ex2.X,'g': ex1.g(ex2.X)},options)
 
 print(rf)
 print(rf(ex1.x0,[]))
